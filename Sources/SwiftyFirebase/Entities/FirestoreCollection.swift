836c27d7ca7e3e9c98d80f2c27b321f2ca5ab077
stecklum <101799038+stecklum@users.noreply.github.com>
2025-02-21 23:13:42 +0100

    feat: improve project structure

---
+++ Sources/SwiftyFirebase/Entities/FirestoreCollection.swift
@@ -0,0 +1,10 @@
+//
+//  FirestoreCollection.swift
+//  
+//
+//  Created by Tom Stecklum on 21.02.25.
+//
+
+public protocol FirestoreCollection {
+    var rawValue: String { get }
+}
---
+++ Sources/SwiftyFirebase/Managers/CloudStorageManager.swift
@@ -0,0 +1,69 @@
+//
+//  CloudStorageManager.swift
+//  
+//
+//  Created by Tom Stecklum on 21.02.25.
+//
+
+import Foundation
+import FirebaseStorage
+
+struct CloudStorageManager {
+    
+    private var storage: Storage
+    
+    init(storage: Storage = Storage.storage()) {
+        self.storage = storage
+    }
+    
+    private func uploadData(_ data: Data, to path: String, completion: @escaping (Result<String, Error>) -> Void) {
+        
+        let storageRef = storage.reference().child(path)
+
+        storageRef.putData(data) { metadata, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(storageRef.fullPath))
+            }
+        }
+    }
+    
+    func uploadData(_ data: Data, to path: String) async throws -> String {
+        try await withCheckedThrowingContinuation { continuation in
+            uploadData(data, to: path) { result in
+                switch result {
+                case .success(let path):
+                    continuation.resume(returning: path)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+
+    private func fetchData(from path: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        let storageRef = storage.reference().child(path)
+
+        storageRef.getData(maxSize: 10 * 1024 * 1024) { data, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data {
+                completion(.success(data))
+            }
+        }
+    }
+    
+    func fetchData(from path: String) async throws -> Data {
+        try await withCheckedThrowingContinuation { continuation in
+            fetchData(from: path) { result in
+                switch result {
+                case .success(let data):
+                    continuation.resume(returning: data)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
