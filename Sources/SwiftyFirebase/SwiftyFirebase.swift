4ea982aca603d2905d66c30fa3c2022169b3cc93
Tom Stecklum <101799038+stecklum@users.noreply.github.com>
2025-03-04 23:19:33 +0100

    feat: exported imports
    * feat: exported imports
    
    * feat: add missing packages

--- Sources/SwiftyFirebase/SwiftyFirebase.swift
+++ Sources/SwiftyFirebase/SwiftyFirebase.swift
@@ -1,6 +1,15 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+@_exported import FirebaseFirestore
+@_exported import FirebaseCrashlytics
+@_exported import FirebaseAuth
+@_exported import FirebaseCore
+@_exported import FirebaseAnalytics
+@_exported import FirebaseMessaging
+@_exported import FirebaseAppCheck
+@_exported import FirebaseStorage
+
 public struct SwiftyFirebase {
     
     public init() {
